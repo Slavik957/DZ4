cb1fe8188565acc78e2b79de655147a1e1f5a9b3
Горбунов Вячеслав <sgorbunov957@gmail.com>
2023-07-07 10:07:45 +0300

    Домашнее задание 25,27,29

---
+++ Задача 27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, 
+//которая принимает на вход число и выдаёт сумму цифр в числе.
+
+   Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        int sum = 0;
+        int temp = number;
+        
+        while (temp != 0)
+        {
+            int digit = temp % 10;
+            sum += digit;
+            
+            temp /= 10;
+        }
+        
+        Console.WriteLine("Сумма цифр в числе {0} равна {1}", number, sum);
